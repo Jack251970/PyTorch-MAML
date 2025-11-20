bde6939f5fcc13a352429a0ac8f002a468e90c42
Jack251970 <1160210343@qq.com>
2025-11-20 16:56:13 +0800

    Update

--- utils/arguments.py
+++ utils/arguments.py
@@ -44,16 +44,16 @@ def parse_launch_parameters():
                              "'Koopa', 'QSQF-C', 'RNN-SF', 'LSTM-CQ', 'LSTM-AQ']")
 
     # data loader
-    parser.add_argument('--data', type=str, default='ETTm1',
+    parser.add_argument('--data', type=str, default='custom',
                         help="dataset type, options: ['ETTh1', 'ETTh2', 'ETTm1', 'ETTm2', 'custom', 'm4', 'PSM', "
                              "'MSL', 'SMAP', 'SMD', 'SWAT', 'UEA']")
-    parser.add_argument('--root_path', type=str, default='./dataset/ETT/', help='root path of the data file')
-    parser.add_argument('--data_path', type=str, default='ETTh1.csv', help='data file')
-    parser.add_argument('--features', type=str, default='M',
+    parser.add_argument('--root_path', type=str, default='./.materials/', help='root path of the data file')
+    parser.add_argument('--data_path', type=str, default='wind/Zone1/Zone1.csv', help='data file')
+    parser.add_argument('--features', type=str, default='MS',
                         help='forecasting task, options:[M, S, MS]; M:multivariate predict multivariate, S:uni-variate '
                              'predict uni-variate, MS:multivariate predict uni-variate')
-    parser.add_argument('--target', type=str, default='OT', help='target feature in S or MS task')
-    parser.add_argument('--freq', type=str, default='h',
+    parser.add_argument('--target', type=str, default='wind', help='target feature in S or MS task')
+    parser.add_argument('--freq', type=str, default='t',
                         help='freq for time features encoding, options:[s:secondly, t:minutely, h:hourly, d:daily, '
                              'b:business days, w:weekly, m:monthly], you can also use more detailed freq like 15min '
                              'or 3h')
@@ -62,12 +62,12 @@ def parse_launch_parameters():
     parser.add_argument('--reindex', type=int, default=0, help='reindex feature dimensions data, 1: enable 0: disable')
     parser.add_argument('--reindex_tolerance', type=float, default=0.9,
                         help='reindex tolerance for feature dimensions data')
-    parser.add_argument('--pin_memory', type=bool, default=True, help='pin memory of data loader')
+    parser.add_argument('--pin_memory', type=bool, default=False, help='pin memory of data loader')
 
     # forecasting task
     parser.add_argument('--seq_len', type=int, default=96, help='input sequence length')
-    parser.add_argument('--label_len', type=int, default=48, help='start token length')
-    parser.add_argument('--pred_len', type=int, default=96, help='prediction sequence length')
+    parser.add_argument('--label_len', type=int, default=96, help='start token length')
+    parser.add_argument('--pred_len', type=int, default=16, help='prediction sequence length')
     parser.add_argument('--seasonal_patterns', type=str, default='Monthly', help='subset for M4')
     parser.add_argument('--inverse', action='store_true', help='inverse output data', default=False)
 
@@ -82,10 +82,10 @@ def parse_launch_parameters():
     parser.add_argument('--d_conv', type=int, default=4, help='conv kernel size for Mamba')
     parser.add_argument('--top_k', type=int, default=5, help='for TimesBlock')
     parser.add_argument('--num_kernels', type=int, default=6, help='for Inception')
-    parser.add_argument('--enc_in', type=int, default=7, help='encoder input size')
-    parser.add_argument('--dec_in', type=int, default=7, help='decoder input size')
-    parser.add_argument('--c_out', type=int, default=7, help='output size')
-    parser.add_argument('--d_model', type=int, default=512, help='dimension of model')
+    parser.add_argument('--enc_in', type=int, default=5, help='encoder input size')
+    parser.add_argument('--dec_in', type=int, default=5, help='decoder input size')
+    parser.add_argument('--c_out', type=int, default=5, help='output size')
+    parser.add_argument('--d_model', type=int, default=128, help='dimension of model')
     parser.add_argument('--n_heads', type=int, default=8, help='num of heads')
     parser.add_argument('--e_layers', type=int, default=2, help='num of encoder layers')
     parser.add_argument('--d_layers', type=int, default=1, help='num of decoder layers')
@@ -118,10 +118,10 @@ def parse_launch_parameters():
     parser.add_argument('--num_workers', type=int, default=12, help='data loader num workers')
     parser.add_argument('--itr', type=int, default=1, help='deprecated')
     parser.add_argument('--train_epochs', type=int, default=10, help='train epochs')
-    parser.add_argument('--batch_size', type=int, default=32, help='batch size of train input data')
+    parser.add_argument('--batch_size', type=int, default=256, help='batch size of train input data')
     parser.add_argument('--patience', type=int, default=3, help='early stopping patience')
     parser.add_argument('--learning_rate', type=float, default=0.0001, help='optimizer learning rate')
-    parser.add_argument('--des', type=str, default='test', help='exp description')
+    parser.add_argument('--des', type=str, default='Exp', help='exp description')
     parser.add_argument('--loss', type=str, default='auto', help='loss function, detect automatically if not set')
     parser.add_argument('--lradj', type=str, default='type1', help='adjust learning rate')
     parser.add_argument('--use_amp', action='store_true', help='use automatic mixed precision training', default=False)
