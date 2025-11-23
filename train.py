2949bde31116d30311637d8c3b40142c98b1a79c
Jack251970 <1160210343@qq.com>
2025-11-23 16:18:24 +0800

    Update

--- train.py
+++ train.py
@@ -183,6 +183,7 @@ def main():
         if aves['vl'] < min_vl:
             min_vl = aves['vl']
             torch.save(ckpt, os.path.join(ckpt_path, 'min-vl.pth'))
+            print(f'Saved epoch {epoch} with min-vl {min_vl}')
 
         writer.flush()
 
