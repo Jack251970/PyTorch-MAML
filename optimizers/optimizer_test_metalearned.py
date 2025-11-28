fa66e0c7f2411898bdb071f461420d4a61fe46dc
Jack251970 <1160210343@qq.com>
2025-11-28 16:00:04 +0800

    Update

--- optimizers/optimizer_test_metalearned.py
+++ optimizers/optimizer_test_metalearned.py
@@ -43,9 +43,6 @@ def get_search_space():
         'data_path': {'_type': 'choice', '_value': [f"filtered_Turbine_Data_Penmanshiel_{i:02d}_2022-01-01_-_2023-01-01.csv"
                                                     for i in [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]]},
         'target': {'_type': 'single', '_value': 'Power (kW)'},
-        'enc_in': {'_type': 'single', '_value': 5},
-        'dec_in': {'_type': 'single', '_value': 5},
-        'c_out': {'_type': 'single', '_value': 5},
     }
 
     learning_config = {
