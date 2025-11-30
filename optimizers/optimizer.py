7ea9298555865ca9b34a380a36f85e74f2a2cfbe
Jack251970 <1160210343@qq.com>
2025-11-30 19:49:15 +0800

    Update

--- optimizers/optimizer.py
+++ optimizers/optimizer.py
@@ -66,7 +66,7 @@ def get_search_space():
     return [default_config, dataset_config, learning_config, period_config], model_configs
 
 
-h = HyperParameterOptimizer(script_mode=False, models=['DLinear'],
+h = HyperParameterOptimizer(script_mode=False, models=['DLinear', 'PatchTST', 'FEDformer'],
                             get_search_space=get_search_space, link_fieldnames_data=link_fieldnames_data)
 h.config_optimizer_settings(root_path='.', data_csv_file='Penmanshiel2022_6112.csv',
-                            scan_all_csv=False, try_model=False, force_exp=True)
+                            scan_all_csv=False, try_model=False, force_exp=False)
